4305fd64f5996bdb7f1015f63c22e1bac1fb720c
Roberto Pacheco <robertoaraujo230@hotmail.com>
2021-08-24 09:45:39 -0300

    Implementando a funcionalidade de autenticação de funcionários

--- Funcionarios/Funcionario.js
+++ Funcionarios/Funcionario.js
@@ -5,5 +5,14 @@ export class Funcionario{
         this._cpf = cpf
 
         this._bonificacao = 1
+        this._senha
+    }
+
+    get senha(){
+        return this._senha
+    }
+
+    cadastrarSenha(novaSenha){
+        this._senha = novaSenha
     }
 }
--- index.js
+++ index.js
@@ -1,14 +1,19 @@
-import { Cliente } from "./Cliente/Cliente.js";
-import { ContaCorrente } from "./Contas/ContaCorrente.js";
-import { ContaPoupanca } from "./Contas/ContaPoupanca.js";
-import { ContaSalario } from "./Contas/ContaSalario.js";
 import { Diretor } from "./Funcionarios/Diretor.js";
 import { Gerente } from "./Funcionarios/Gerente.js";
-
+import { SistemaAutenticacao } from "./Autenticacao/SistemaAutenticacao.js";
 
 const diretor1 = new Diretor('Carla', 7000, 15748234176)
 
 const gerente1 = new Gerente('Ana', 5000, 92354178603)
 
+diretor1.cadastrarSenha('345')
+gerente1.cadastrarSenha('789')
+
 console.log(diretor1)
 console.log(gerente1)
+
+const estaLogado1 = SistemaAutenticacao.login(diretor1, '123')
+const estaLogado2 = SistemaAutenticacao.login(gerente1, '789')
+
+console.log(estaLogado1)
+console.log(estaLogado2)
